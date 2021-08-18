5fdf1c6c5475475508cb8c29ed98719c2b64fed7
Patrikk Dyrberg Sørensen <patri-kk@hotmail.com>
2021-08-18 10:53:49 +0200

    Update s3-event-listener.js

--- week4/materials/hyf-week-4-event-lambda/src/handlers/s3-event-listener.js
+++ week4/materials/hyf-week-4-event-lambda/src/handlers/s3-event-listener.js
@@ -1,26 +1,25 @@
 const AWS = require('aws-sdk');
-
 const s3 = new AWS.S3();
+
+//todo: add https://github.com/C2FO/fast-csv
 
 /**
  * A Lambda function that logs the payload received from S3.
  */
 exports.s3JsonLoggerHandler = async (event, context) => {
-  // All log statements are written to CloudWatch by default. For more information, see
-  // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-  const getObjectRequests = event.Records.map(record => {
+  event.Records.forEach(record => {
     const params = {
       Bucket: record.s3.bucket.name,
       Key: record.s3.object.key
     };
-    return s3.getObject(params).promise().then(data => {
-      console.info(data.Body.toString());
-    }).catch(err => {
-      console.error("Error calling S3 getObject:", err);
-      return Promise.reject(err);
-    })
-  });
-  return Promise.all(getObjectRequests).then(() => {
-    //console.debug('Complete!');
+    
+    // get csv file and create stream
+    const stream = S3.getObject(params).createReadStream();
+    
+    let parser = csv.fromStream(stream, { headers: true }).on("data", function (data) {
+        console.log(data);
+    }).on("end", function () {
+        console.log('process finished');
+    });
   });
 };
