ce269e7632a848940374ef37f6e378db550d4973
Patrikk Sorensen <patrikk@audienceproject.com>
2021-08-27 23:32:49 +0200

    Week5

--- week5/materials/good-green-groceries-api/src/handlers/get-all-items.js
+++ week5/materials/good-green-groceries-api/src/handlers/get-all-items.js
@@ -1,24 +1,26 @@
 const tableName = process.env.PRODUCTS_TABLE;
 
-const dynamodb = require('aws-sdk/clients/dynamodb');
+const dynamodb = require("aws-sdk/clients/dynamodb");
 const docClient = new dynamodb.DocumentClient();
 
 exports.getAllItemsHandler = async (event) => {
-    // All log statements are written to CloudWatch
-    console.info('received:', event);
+  // All log statements are written to CloudWatch
+  console.info("received:", event);
 
-    var params = {
-        TableName: "good-green-groceries-products"
-    };
-    const data = await docClient.scan(params).promise();
-    const items = data.Items;
+  var params = {
+    TableName: "?",
+  };
+  const data = await docClient.scan(params).promise();
+  const items = data.Items;
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(items)
-    };
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(items),
+  };
 
-    // All log statements are written to CloudWatch
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
-}
+  // All log statements are written to CloudWatch
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+  return response;
+};
--- week5/preparation.md
+++ week5/preparation.md
@@ -1,6 +1,12 @@
- # Preparation
-- books, articles, videos, podcasts, online courses that are mandatory (mandatory)
-- and others are might be optional (optional)
+# Preparation
 
-## Flipped classroom videos
-- N/A
+## Main
+
+- last weeks assignments
+- read lesson plan
+- read [Using API Gateway as a Single Entry Point for Web Applications and API Microservices](https://aws.amazon.com/blogs/architecture/using-api-gateway-as-a-single-entry-point-for-web-applications-and-api-microservices/)
+
+## Relevant articles (optional)
+
+- [The Serverless Framework](https://www.serverless.com/about)
+- [The lambda architecture](https://en.wikipedia.org/wiki/Lambda_architecture)
